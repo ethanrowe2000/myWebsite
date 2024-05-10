70f78dba5c8d0051cc5add11ea298e058d22f3b1
ethanrowe2000 <140987141+ethanrowe2000@users.noreply.github.com>
2024-05-09 20:42:26 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+//For the first set of buttons
+function show1(){
+    document.getElementById('first').style.gridTemplateRows =  "1520px" ;
+    document.getElementById('showButton1').style.display = "none";
+    document.getElementById('hideButton1').style.display = "block";
+}
+function hide1(){
+    document.getElementById('first').style.gridTemplateRows =  "399px" ;
+    document.getElementById('showButton1').style.display = "block";
+    document.getElementById('hideButton1').style.display = "none";
+}
+
+//For the second set of buttons
+function show2(){
+    document.getElementById('second').style.gridTemplateRows =  "1395px" ;
+    document.getElementById('showButton2').style.display = "none";
+    document.getElementById('hideButton2').style.display = "block";
+}
+function hide2(){
+    document.getElementById('second').style.gridTemplateRows =  "399px" ;
+    document.getElementById('showButton2').style.display = "block";
+    document.getElementById('hideButton2').style.display = "none";
+}
+
+//For the third set of buttons
+function show3(){
+    document.getElementById('third').style.gridTemplateRows =  "940px" ;
+    document.getElementById('showButton3').style.display = "none";
+    document.getElementById('hideButton3').style.display = "block";
+}
+function hide3(){
+    document.getElementById('third').style.gridTemplateRows =  "399px" ;
+    document.getElementById('showButton3').style.display = "block";
+    document.getElementById('hideButton3').style.display = "none";
+}
+
+//For the fourth set of buttons
+function show4(){
+    document.getElementById('fourth').style.gridTemplateRows =  "670px" ;
+    document.getElementById('showButton4').style.display = "none";
+    document.getElementById('hideButton4').style.display = "block";
+}
+function hide4(){
+    document.getElementById('fourth').style.gridTemplateRows =  "399px" ;
+    document.getElementById('showButton4').style.display = "block";
+    document.getElementById('hideButton4').style.display = "none";
+}
