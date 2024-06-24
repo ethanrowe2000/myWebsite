6696d86b9d26c364b5bb8e24d4100c5d64e1941e
ethanrowe2000 <140987141+ethanrowe2000@users.noreply.github.com>
2024-06-24 19:26:33 -0400

    Updating Website

--- script.js
+++ script.js
@@ -1,48 +1,59 @@
+function show0(){
+    document.getElementById('zero').style.gridTemplateRows =  "1375px" ;
+    document.getElementById('showButton0').style.display = "none";
+    document.getElementById('hideButton0').style.display = "block";
+}
+function hide0(){
+    document.getElementById('zero').style.gridTemplateRows =  "190px" ;
+    document.getElementById('showButton0').style.display = "block";
+    document.getElementById('hideButton0').style.display = "none";
+}
+
 
 //For the first set of buttons
 function show1(){
-    document.getElementById('first').style.gridTemplateRows =  "1520px" ;
+    document.getElementById('first').style.gridTemplateRows =  "1325px" ;
     document.getElementById('showButton1').style.display = "none";
     document.getElementById('hideButton1').style.display = "block";
 }
 function hide1(){
-    document.getElementById('first').style.gridTemplateRows =  "399px" ;
+    document.getElementById('first').style.gridTemplateRows =  "190px" ;
     document.getElementById('showButton1').style.display = "block";
     document.getElementById('hideButton1').style.display = "none";
 }
 
 //For the second set of buttons
 function show2(){
-    document.getElementById('second').style.gridTemplateRows =  "1395px" ;
+    document.getElementById('second').style.gridTemplateRows =  "1055px" ;/*The length of the experience box when Show more button is clicked.*/
     document.getElementById('showButton2').style.display = "none";
     document.getElementById('hideButton2').style.display = "block";
 }
 function hide2(){
-    document.getElementById('second').style.gridTemplateRows =  "399px" ;
+    document.getElementById('second').style.gridTemplateRows =  "190px" ;
     document.getElementById('showButton2').style.display = "block";
     document.getElementById('hideButton2').style.display = "none";
 }
 
 //For the third set of buttons
 function show3(){
-    document.getElementById('third').style.gridTemplateRows =  "940px" ;
+    document.getElementById('third').style.gridTemplateRows =  "750px" ;
     document.getElementById('showButton3').style.display = "none";
     document.getElementById('hideButton3').style.display = "block";
 }
 function hide3(){
-    document.getElementById('third').style.gridTemplateRows =  "399px" ;
+    document.getElementById('third').style.gridTemplateRows =  "190px" ;
     document.getElementById('showButton3').style.display = "block";
     document.getElementById('hideButton3').style.display = "none";
 }
 
 //For the fourth set of buttons
 function show4(){
-    document.getElementById('fourth').style.gridTemplateRows =  "670px" ;
+    document.getElementById('fourth').style.gridTemplateRows =  "620px" ;
     document.getElementById('showButton4').style.display = "none";
     document.getElementById('hideButton4').style.display = "block";
 }
 function hide4(){
-    document.getElementById('fourth').style.gridTemplateRows =  "399px" ;
+    document.getElementById('fourth').style.gridTemplateRows =  "190px" ;
     document.getElementById('showButton4').style.display = "block";
     document.getElementById('hideButton4').style.display = "none";
 }
